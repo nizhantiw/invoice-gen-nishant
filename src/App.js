3f3b562fecc91573e4a0b2ceaa52a565b30a9634
nizhantiw <nizhantiw@gmail.com>
2023-01-21 12:27:04 +0530

    final commit nishant
    Invoice generator for shopping list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Component } from 'react';
+import Container  from 'react-bootstrap/Container';
+import InvoiceForm from './components/InvoiceForm';
 
-function App() {
+
+
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex flex-column align-items-center justify-content-center w-100">
+      <Container>
+        <InvoiceForm/>
+      </Container>
     </div>
   );
-}
+}}
 
 export default App;
